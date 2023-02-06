de03b77e1a9414029d48597d2abdf1602a0b1517
Abdulrahman Abdullah <76687899+AbdulrahmanAlGhofaily@users.noreply.github.com>
2023-02-06 13:53:21 +0300

    Update config.ts

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -1,21 +1,24 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 dotenv.config();
 
-// ENV variables 
+// ENV variables
 // - AWS_ACCESS_KEY_ID
 // - AWS_SECRET_ACCESS_KEY
 // Are Also needed
 
 export const config = {
-  username: `${process.env.POSTGRES_USERNAME}`,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  host: process.env.POSTGRES_HOST,
-  aws_region: process.env.AWS_REGION,
-  aws_profile: process.env.AWS_PROFILE,
-  aws_media_bucket: process.env.AWS_BUCKET,
-  url: process.env.URL,
-  jwt: {
+  POSTGRES_USERNAME: `${process.env.POSTGRES_USERNAME}`,
+  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+  POSTGRES_DB: process.env.POSTGRES_DB,
+  POSTGRES_HOST: process.env.POSTGRES_HOST,
+  AWS_REGION: process.env.AWS_REGION,
+  AWS_DEFAULT_REGION: process.env.AWS_DEFAULT_REGION,
+  AWS_PROFILE: process.env.AWS_PROFILE,
+  AWS_BUCKET: process.env.AWS_BUCKET,
+  URL: process.env.URL,
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  JWT_SECRET: {
     secret: process.env.JWT_SECRET,
   },
 };
