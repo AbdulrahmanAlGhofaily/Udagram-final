bfefb8b488bb2217a9decbd76f102377009a65a2
Abdulrahman Abdullah <76687899+AbdulrahmanAlGhofaily@users.noreply.github.com>
2023-02-06 13:54:34 +0300

    Update sequelize.ts

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,12 +1,12 @@
-import { Sequelize } from "sequelize-typescript";
-import { config } from "./config/config";
+import { Sequelize } from 'sequelize-typescript';
+import { config } from './config/config';
 
 export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
+  username: config.POSTGRES_USERNAME,
+  password: config.POSTGRES_PASSWORD,
+  database: config.POSTGRES_DB,
+  host: config.POSTGRES_HOST,
 
-  dialect: "postgres",
-  storage: ":memory:",
+  dialect: 'postgres',
+  storage: ':memory:',
 });
